c4d8174fdd0b50fef2ab9297442b4f7693f82bb6
lucaspbueno <lucaspbueno22@gmail.com>
2025-08-27 07:52:57 -0300

    feat: adiciona modelo Posicao e atualiza __all__ no init de models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,15 @@
 from .lote import Lote
+from .posicao import Posicao
+from .ativo import Ativo
+from .indexador import Indexador
+from .fundo_investimento import FundoInvestimento
+from .orgao_financeiro import OrgaoFinanceiro
 
-__all__ = ["Lote"]
+__all__ = [
+    "Lote",
+    "Posicao",
+    "Ativo",
+    "Indexador",
+    "FundoInvestimento",
+    "OrgaoFinanceiro"
+]
