86d16d7998e75a05e201e940a1d94a6d7c51223a
lucaspbueno <lucaspbueno22@gmail.com>
2025-08-27 00:13:57 -0300

    feat: adiciona classes Base e TimestampMixin para estruturação de modelos

---
+++ src/models/utils/timestamp_mixin.py
@@ -0,0 +1,18 @@
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy import DateTime
+from typing import Optional
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+        onupdate=func.now()
+    )
