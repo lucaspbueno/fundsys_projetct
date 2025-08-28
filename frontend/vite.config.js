9a3f733a4e32e8b967ce30fea3eb8fdf16ed2c7b
lucaspbueno <lucaspbueno22@gmail.com>
2025-08-28 16:33:33 -0300

    feat: ajusta Dockerfile e docker-compose para que o front suba na porta 5500

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwind from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  server: {
+    port: 5500,
+    host: true
+  }
 })
