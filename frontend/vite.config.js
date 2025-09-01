0c296c0c2a4266c9a04a647bc3207c00ab762c7f
lucaspbueno <lucaspbueno22@gmail.com>
2025-09-01 00:39:21 -0300

    feat: add shadcn ao projeto e a configuracao necessaria para que seus componentes funcionem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-// https://vite.dev/config/
+// ESM: cria __filename / __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react(), tailwind()],
   server: {
     port: 5500,
-    host: true
-  }
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
