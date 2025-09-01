ba450c946a6c311c51cc3b4e1d25352141e0fa35
lucaspbueno <lucaspbueno22@gmail.com>
2025-09-01 17:31:25 -0300

    feat: adiciona um layout para a página principal da aplicação, a Home

---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
+}
