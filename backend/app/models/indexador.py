20ef4abe8fd24318c56372a1c9085248e6a346d2
lucaspbueno <lucaspbueno22@gmail.com>
2025-09-10 09:01:46 -0300

    feat: transforma coluna sgl_indexador em opcional e permite valores nulos na tabela tb_indexador

--- backend/app/models/indexador.py
+++ backend/app/models/indexador.py
@@ -1,7 +1,7 @@
 from .utils import BaseModel, TimestampMixin
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from .ativo import Ativo
@@ -9,8 +9,8 @@ if TYPE_CHECKING:
 class Indexador(BaseModel, TimestampMixin):
     __tablename__ = "tb_indexador"
 
-    id_indexador : Mapped[int] = mapped_column(Integer, primary_key=True)
-    cd_indexador : Mapped[str] = mapped_column(String(100), nullable=False)
-    sgl_indexador: Mapped[str] = mapped_column(String(100), nullable=False)
+    id_indexador : Mapped[int]           = mapped_column(Integer, primary_key=True)
+    cd_indexador : Mapped[str]           = mapped_column(String(100), nullable=False)
+    sgl_indexador: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
 
     ativos: Mapped[list["Ativo"]] = relationship("Ativo", back_populates="indexador")
