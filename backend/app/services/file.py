b65eed2e43661f80d0a8cdd83254b1d0db61e383
lucaspbueno <lucaspbueno22@gmail.com>
2025-09-10 09:08:49 -0300

    feat: refatora serviço de upload de arquivos para incluir tratamento de múltiplos títulos privados e persistência de bundles

--- backend/app/services/file.py
+++ backend/app/services/file.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from typing import Any, Dict, List
 from app.DTOs import ParsedBundleDTO, AtivoDTO, IndexadorDTO, LoteDTO, PosicaoDTO
 from app.utils import FileLoader, Parser, convert_to_list, str_to_datetime_utc, str_to_decimal, str_to_float
+from app.persiste import persist_bundles
 
 
 async def upload_files_service(
@@ -21,48 +22,51 @@ async def upload_files_service(
     bundles: List[ParsedBundleDTO] = []
 
     for file in ls_files:
-        text   = await loader.load_text(file)
+        text = await loader.load_text(file)
         doc: Dict[str, Any] = parser.parse_xml_text_to_dict(text) or {}
-        root   = doc.get("arquivoposicao_4_01", {})
+        root = doc.get("arquivoposicao_4_01", {})
         fundos = convert_to_list(root.get("fundo"))
 
-        for a in fundos:
-            # --- construir entidades individuais ---
-            lote = LoteDTO(
-                vl_pu_compra = str_to_decimal(a.get("pucompra")),
-                qtd_comprada = str_to_decimal(a.get("qtdisponivel")),
-                dt_operacao  = str_to_datetime_utc(a.get("dtoperacao")),
-            )
-
-            indexador = IndexadorDTO(
-                cd_indexador  = a.get("indexador"),
-                sgl_indexador = a.get("sgl_indexador"),
-            )
-
-            ativo = AtivoDTO(
-                cd_ativo       = a.get("codativo"),
-                cd_isin        = a.get("isin"),
-                vl_pu_emissao  = str_to_decimal(a.get("puemissao")),
-                perc_indexador = str_to_float(a.get("percindex")),
-                perc_cupom     = str_to_float(a.get("perccupom")),
-                dt_emissao     = str_to_datetime_utc(a.get("dtemissao")),
-                dt_vencimento  = str_to_datetime_utc(a.get("dtvencimento")),
-            )
-
-            posicao = PosicaoDTO(
-                vl_pu_posicao            = str_to_decimal(a.get("puposicao")),
-                vl_principal             = str_to_decimal(a.get("principal")),
-                vl_financeiro_disponivel = str_to_decimal(a.get("valorfindisp")),
-                dt_posicao               = str_to_datetime_utc(a.get("dtoperacao")),
-            )
-
-            bundles.append(
-                ParsedBundleDTO(
-                    lote      = lote,
-                    indexador = indexador,
-                    ativo     = ativo,
-                    posicao   = posicao,
+        for fundo in fundos:
+            titprivado_list = convert_to_list(fundo.get("titprivado", []))
+            
+            for titprivado in titprivado_list:
+                # --- construir entidades individuais ---
+                lote = LoteDTO(
+                    vl_pu_compra = str_to_decimal(titprivado.get("pucompra")),
+                    qtd_comprada = str_to_decimal(titprivado.get("qtdisponivel")),
+                    dt_operacao  = str_to_datetime_utc(titprivado.get("dtoperacao")),
                 )
-            )
 
-    return bundles
+                indexador = IndexadorDTO(
+                    cd_indexador  = titprivado.get("indexador"),
+                    sgl_indexador = titprivado.get("sgl_indexador", ""),
+                )
+
+                ativo = AtivoDTO(
+                    cd_ativo       = titprivado.get("codativo"),
+                    cd_isin        = titprivado.get("isin"),
+                    vl_pu_emissao  = str_to_decimal(titprivado.get("puemissao")),
+                    perc_indexador = str_to_float(titprivado.get("percindex")),
+                    perc_cupom     = str_to_float(titprivado.get("coupom")),
+                    dt_emissao     = str_to_datetime_utc(titprivado.get("dtemissao")),
+                    dt_vencimento  = str_to_datetime_utc(titprivado.get("dtvencimento")),
+                )
+
+                posicao = PosicaoDTO(
+                    vl_pu_posicao            = str_to_decimal(titprivado.get("puposicao")),
+                    vl_principal             = str_to_decimal(titprivado.get("principal")),
+                    vl_financeiro_disponivel = str_to_decimal(titprivado.get("valorfindisp")),
+                    dt_posicao               = str_to_datetime_utc(titprivado.get("dtoperacao")),
+                )
+
+                bundles.append(
+                    ParsedBundleDTO(
+                        lote      = lote,
+                        indexador = indexador,
+                        ativo     = ativo,
+                        posicao   = posicao,
+                    )
+                )
+
+    return persist_bundles(db, bundles)
